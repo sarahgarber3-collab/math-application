f492b56b4d46f0fc7a0f557e3ade4684d8b2f98e
sarahgarber3@gmail.com <sarahgarber3@gmail.com>
2026-04-07 15:48:28 -0400

    Math questions and some answers.

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -1,4 +1,36 @@
 package com.pluralsight;
 
 public class MathApp {
+    public static void main(String[] args) {
+        //Question 1
+        //Variables for question 1
+        int bobSalary = 45000;
+        int garySalary = 60000;
+        String highestSalary = "";
+
+        //who's salary is greater?
+
+
+
+        //Question 2
+        //Variables
+        int carPrice = 26000;
+        int truckPrice = 80000;
+
+        //Question 3
+
+        //Question 4
+
+        //Question 5
+
+        //Question 6
+
+        //Question 7
+
+        //Question 8
+
+
+
+
+    }
 }
