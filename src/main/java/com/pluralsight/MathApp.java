8d1c808ab808210674dbb12d86f59dd8cd464a32
sarahgarber3@gmail.com <sarahgarber3@gmail.com>
2026-04-08 11:05:00 -0400

    answers for question 4

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -3,40 +3,52 @@ package com.pluralsight;
 public class MathApp {
     public static void main(String[] args) {
         //Question 1
+        //
         //Variables for question 1
         int bobSalary = 45000;
         int garySalary = 60000;
-
+        int highestSalary = Math.max(bobSalary,garySalary);
 
         //whose salary is greater?
-        int highestSalary = Math.max(bobSalary,garySalary);
-        System.out.println("the maximum is " + highestSalary);
 
+        System.out.println("the maximum is $" + highestSalary);
 
         //Question 2
+        //which variable is lower?
         //Variables
         int carPrice = 26000;
         int truckPrice = 80000;
-
         int lowestPrice = Math.min(carPrice,truckPrice);
-        System.out.println("The minimum is " + lowestPrice);
+
+        System.out.println("The minimum is $" + lowestPrice);
 
         //Question 3
+        //Variable
         double radius = 7.25;
 
+        //Answer
         System.out.println("the area is " + Math.PI * (radius * radius) );
 
-
         //Question 4
+        // Find and display the square root a variable after it is set to 5.0
+        //Variable
+        double number = 5.0;
+        System.out.println("Square root of number 5.0 is " + Math.sqrt(number));
+
 
 
         //Question 5
+        //Find and display the distance between the points (5, 10) and (85, 50)
 
         //Question 6
+        //Find and display the absolute (positive) value of a variable after it is set to -3.8
 
         //Question 7
+        //Find and display the absolute (positive) value of a variable after it is set to -3.8
 
         //Question 8
+        //. Calculate how many minutes are in 24 days, use a variable for each value you
+        //calculate with. BONUS: How many milliseconds?
 
 
 
